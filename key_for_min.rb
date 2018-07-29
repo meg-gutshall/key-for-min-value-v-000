063696239b5d7d017f599bf0b8ea147e715443b5
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-29 01:35:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = nil
   name_hash.each do |key, value|
-    if min_value == nil
-      min_value = value
-    elsif min_value > value
+    if min_value == nil || min_value > value
       min_value = value
     end
   end
